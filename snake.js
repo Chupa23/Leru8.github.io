1869c0f804b39349fd173fbb30bb48db149a55c3
Leru8 <36373366+Leru8@users.noreply.github.com>
2018-07-21 15:26:24 +0300

    r

--- snake.js
+++ snake.js
@@ -1,22 +1,31 @@
 cvs=document.getElementById('canvas');
 ctx=cvs.getContext('2d');
 
-document.addEventListener('keydown' ,setDirectionK)
-var tmOut;
+document.addEventListener('keypress' ,setDirectionK)
+
 var score=0;
 var box=17;
 var snake=[];
 var game=true;
 var d ;
-
+var j=0;
+var food = {
+    x:Math.floor((Math.random()*20))*box,
+    y:Math.floor((Math.random()*20))*box
+ }
+ var foodImg = new Image;
+ foodImg.src=('food.png')
+snake[0]= {
+    x:2*box,y:2*box
+}
 
 function setDirectionK(event){
-  var key=event.keyCode;
+    var key=event.keyCode;   
   if (key==37&&d!='right'){d='left'}
   if (key==38&&d!='down'){d='up'}
   if (key==39&&d!='left'){d='right'}
   if (key==40&&d!='up'){d='down'}
-  if (key==82){restart()}
+  if (key==82){restart()}  
 }
 
 function setDirectionB(dir){
@@ -24,15 +33,6 @@ function setDirectionB(dir){
     if (dir=='up'&&d!='down'){d='up'}
     if (dir=='right'&&d!='left'){d='right'}
     if (dir=='down'&&d!='up'){d='down'}
-}
-
-snake[0]= {
-    x:2*box,y:2*box
-}
-
-var food = {
-   x:(Math.floor((Math.random()*18+1)))*box,
-   y:Math.floor((Math.random()*18+1))*box
 }
 
 function restart(){
@@ -43,36 +43,37 @@ function restart(){
     d='';
     game = true
     food = {
-        x:(Math.floor((Math.random()*18+1)))*box,
-        y:Math.floor((Math.random()*18+1))*box
+        x:Math.floor((Math.random()*20))*box,
+        y:Math.floor((Math.random()*20))*box
      }
-    animate = setInterval(draw,90);
-    snake[0]= {
+     snake[0]= {
         x:2*box,y:2*box
-    }}
-    animation();
+    }
+    animate = setInterval(draw,90);
+} 
 }
+
+
 //draw snake---------------------------------------------------
 function draw(){
-    ctx.fillStyle='grey'
-    ctx.fillRect(0,0,innerWidth,innerHeight)
+    ctx.fillStyle='#631723';
+    ctx.fillRect(0,0,innerWidth,innerHeight);
    for(var i=0; i<snake.length;i++){
-       ctx.fillStyle=(i==0)?'black':'white';
+       ctx.fillStyle=(i==0)?'#053138':'#0C2733';
        ctx.fillRect(snake[i].x,snake[i].y,box,box)
-       ctx.strokeStyle='black';
-       ctx.strokeRect(snake[i].x,snake[i].y,box,box)
    }
-ctx.fillStyle='maroon'
-ctx.fillRect(food.x,food.y,box,box)
+
+ctx.drawImage(foodImg,food.x,food.y,box,box);
    
    var snakeX=snake[0].x;
    var snakeY=snake[0].y;
 
-
+   
    if (d=='left'){snakeX-=box;}
    if (d=='up'){snakeY-=box;}
    if (d=='right'){snakeX+=box;}
    if (d=='down'){snakeY+=box;}
+ 
 
  if (snakeX==-box||snakeX==20*box||snakeY==-box||snakeY==20*box)
  {game=false;}
@@ -80,6 +81,9 @@ ctx.fillRect(food.x,food.y,box,box)
  for (var i = 1;i<snake.length;i++) {
      if (snakeX==snake[i].x&&snakeY==snake[i].y){game=false;}
  }
+
+ var newHead = {x:snakeX,y:snakeY};
+ snake.unshift(newHead);
 if (snakeX==food.x&&snakeY==food.y){
     score++;
     sc=score.toString()
@@ -93,18 +97,11 @@ else {
  snake.pop();
 }
 
- var newHead = {x:snakeX,y:snakeY}
- snake.unshift(newHead)
- console.log(score)
+
+ 
 
 }
 
-function checkDir(){
-   if (d=='left'){draw.snakeX-=box;}
-   if (d=='up'){draw.snakeY-=box;}
-   if (d=='right'){draw.snakeX+=box;}
-   if (d=='down'){draw.snakeY+=box;}
-}
 //------------------------------------------------------------
 
 var animate = setInterval(draw,90);
@@ -113,4 +110,4 @@ function gameOver(){
     if (!game){clearInterval(animate)}
 }
 var anm = setInterval(gameOver, 90);
-var dirChg= setInterval(checkDir , 90)
+
