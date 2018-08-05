792eb3ebee3ff51f8670e5294a2446a571a2dc0b
Leru8 <36373366+Leru8@users.noreply.github.com>
2018-08-05 16:37:35 +0300

    sp

---
+++ Snake/pass.js
@@ -0,0 +1,11 @@
+function pass(){  
+    var nOyB = document.getElementById('ps').value;
+    var link = document.getElementById('DlB');
+    if (nOyB=='pentrucadecenu...') {
+        link.style.display='inline';
+    }
+    else {
+        link.style.display='none';
+    }
+    console.log(nOyB)
+}
